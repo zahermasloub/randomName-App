2e11ad18e3ecdaa3c9a7342b30f6150b9460273d
zaher <crc.qa2222@gmail.com>
2020-06-19 10:25:45 +0300

    first commit

---
+++ random.js
@@ -0,0 +1,39 @@
+//import HTML Item 
+const UIform = document.querySelector(".card");
+const inpot1 = document.getElementById("b1");
+const inpot2 = document.querySelector(".inbt2");
+const btn = document.querySelector(".sbmit");
+const UIAddname = document.querySelector(".addName");
+
+
+//Event Handeler 
+
+UIAddname.addEventListener("click", addNametext)
+
+
+// functions 
+
+function addNametext(e) {
+
+    const arrData = inpot1.value
+
+    let text1;
+    if (localStorage.getItem("text1") === null) {
+
+        text1 = [];
+
+    } else {
+        text1 = JSON.parse(localStorage.getItem("text1"));
+
+    }
+    text1.push(arrData);
+
+
+
+    localStorage.setItem("name", JSON.stringify(text1));
+
+    alert("SAVED");
+
+    e.preventDefault();
+
+}
